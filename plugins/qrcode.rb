c93d0ba190d1c1a998582e0be5f32c36c72056f7
Whirlwind <whirlwindjames@foxmail.com>
2013-06-05 19:36:21 +0800

    [Plugin]change QRCode style

--- plugins/qrcode.rb
+++ plugins/qrcode.rb
@@ -10,7 +10,7 @@ module Jekyll
             if !$1.nil?
                 @size = $1.strip
             else
-                @size = "100x100"
+                @size = "130x130"
             end
         end
         @img = ImageTag.new('img', $2, nil)
@@ -20,7 +20,6 @@ module Jekyll
     def render(context)
         url = @img.img_info['src']
         @img.img_info['src'] = "http://chart.apis.google.com/chart?chs=#{@size}&cht=qr&chld=|0&chco=165B94&chl=#{url.start_with?('/') ? context.registers[:site].config['url']+url : url}"
-        @img.img_info['style'] = "border:.2em #fff solid"
         "<a href=#{url}>#{@img.render(context)}</a>"
     end
 
